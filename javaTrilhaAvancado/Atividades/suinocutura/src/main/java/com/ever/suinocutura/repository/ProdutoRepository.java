e3120dac135a632bf9d0f58b47f68280be220e12
SnugbXReisdencia <santoseverlan45@gmail.com>
2024-03-12 11:27:16 -0300

    add Repository produto

---
+++ javaTrilhaAvancado/Atividades/suinocutura/src/main/java/com/ever/suinocutura/repository/ProdutoRepository.java
@@ -0,0 +1,9 @@
+package com.ever.suinocutura.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ever.suinocutura.model.Produto;
+
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+
+}
