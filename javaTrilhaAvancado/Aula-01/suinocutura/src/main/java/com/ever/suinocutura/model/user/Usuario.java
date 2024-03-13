34f72a5e3205433461d3073ccd8bb3a56b55ea1e
SnugbXResidência <santoseverlan45@gmail.com>
2024-03-13 17:02:18 -0300

    add dependencia de documentação

--- javaTrilhaAvancado/Aula-01/suinocutura/src/main/java/com/ever/suinocutura/model/user/Usuario.java
+++ javaTrilhaAvancado/Aula-01/suinocutura/src/main/java/com/ever/suinocutura/model/user/Usuario.java
@@ -1,76 +1,79 @@
-package com.ever.suinocutura.model.user;
-
-import java.util.Collection;
-
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-@Entity(name = "users")
-@Getter
-@NoArgsConstructor
-@AllArgsConstructor
-public class Usuario implements UserDetails{
-	
-	@Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	
-	private String login;
-	
-	private String password;
-	
-	private UserRole role;
-
-	@Override
-	public Collection<? extends GrantedAuthority> getAuthorities() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public String getPassword() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public String getUsername() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public boolean isAccountNonExpired() {
-		// TODO Auto-generated method stub
-		return true;
-	}
-
-	@Override
-	public boolean isAccountNonLocked() {
-		// TODO Auto-generated method stub
-		return true;
-	}
-
-	@Override
-	public boolean isCredentialsNonExpired() {
-		// TODO Auto-generated method stub
-		return true;
-	}
-
-	@Override
-	public boolean isEnabled() {
-		// TODO Auto-generated method stub
-		return true;
-	}
-	
-	
-	
-}
+//package com.ever.suinocutura.model.user;
+//
+//import java.util.Collection;
+//import java.util.List;
+//
+//import org.springframework.security.core.GrantedAuthority;
+//import org.springframework.security.core.authority.SimpleGrantedAuthority;
+//import org.springframework.security.core.userdetails.UserDetails;
+//
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.GeneratedValue;
+//import jakarta.persistence.GenerationType;
+//import jakarta.persistence.Id;
+//import lombok.AllArgsConstructor;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//
+//@Entity(name = "users")
+//@Getter
+//@NoArgsConstructor
+//@AllArgsConstructor
+//public class Usuario implements UserDetails{
+//		
+//	@Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//	private Long id;
+//	
+//	private String login;
+//	
+//	private String password;
+//	
+//	private UserRole role;
+//
+//	@Override
+//	public Collection<? extends GrantedAuthority> getAuthorities() {
+//		if(this.role == UserRole.ADMIN) {
+//			return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"), new SimpleGrantedAuthority("ROLE_USER"));
+//		}else {
+//			return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+//		}
+//	}
+//
+//	@Override
+//	public String getPassword() {
+//		return this.password;
+//	}
+//
+//	@Override
+//	public String getUsername() {
+//		return this.login;
+//	}
+//
+//	@Override
+//	public boolean isAccountNonExpired() {
+//		// TODO Auto-generated method stub
+//		return true;
+//	}
+//
+//	@Override
+//	public boolean isAccountNonLocked() {
+//		// TODO Auto-generated method stub
+//		return true;
+//	}
+//
+//	@Override
+//	public boolean isCredentialsNonExpired() {
+//		// TODO Auto-generated method stub
+//		return true;
+//	}
+//
+//	@Override
+//	public boolean isEnabled() {
+//		// TODO Auto-generated method stub
+//		return true;
+//	}
+//	
+//	
+//	
+//}
